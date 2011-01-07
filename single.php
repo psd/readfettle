e616fc5c70b078a6f687f5bdc70cf99ed34782cf
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2011-01-07 16:50:46 +0000

    added Edit

--- single.php
+++ single.php
@@ -6,6 +6,7 @@
 	<div id="footer">
 	<p>
 		A <a href="/">Whatfettle</a> post by <a href="/about/">Paul Downey</a> published on the <?php the_time('l, F jS, Y,') ?> at <?php the_time() ?> under a <a id="license" rel="license" href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons License">Creative Commons License</a>.</p>
+		<?php edit_post_link('<p>Edit</p>'); ?>
 	</div>
 	<?php comments_template(); ?>
 </div>
