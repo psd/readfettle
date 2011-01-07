15d5240305a9d09f93b1aa2ecd23f4ed6026bb59
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2011-01-07 16:57:19 +0000

    resurected page for about

--- page.php
+++ page.php
@@ -1,1 +1,8 @@
-<?php header("Location: /", TRUE, 301); ?>
+<?php get_header(); ?>
+<div id="content">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<h2><a href="/">Whatfettle</a> &#8275; <a href="<?php the_permalink()  ?>"><?php the_title(); ?></a></h2>
+	<div class="post"><?php the_content(); ?></div>
+<?php endwhile; endif; ?>
+</div>
+<?php get_footer(); ?>
