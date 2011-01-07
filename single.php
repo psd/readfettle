b30a12607da8faa8593455f38e8d026976c2caf7
Paul Downey <pauldowney@Unknown-00-25-00-fa-00-70.home>
2011-01-07 07:40:29 +0000

    post wrapper

--- single.php
+++ single.php
@@ -2,7 +2,9 @@
 <div id="content">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<h2><?php the_title(); ?></h2>
+	<div class="post">
 	<?php the_content('<span class="more-link">Continue Reading &raquo;</span>'); ?>
+	</div>
 	<div id="footer">
 		<p>A <a href="/">Whatfettle</a> post by <a href="/about/">Paul Downey</a> published on the <?php the_time('l, F jS, Y,') ?> at <?php the_time() ?> under a <a id="license" rel="license" href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons License">Creative Commons License</a>.</p>
 	</div>
