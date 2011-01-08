70e09a63019a515fefbaef103dbe3ea680bf99c2
Paul Downey <pauldowney@Unknown-00-25-00-fa-00-70.home>
2011-01-08 11:25:55 +0000

    edit this page

--- page.php
+++ page.php
@@ -3,6 +3,6 @@
 	<?php the_post(); ?>
 	<h2><a href="/">Whatfettle</a> &#8275; <a href="<?php the_permalink()  ?>"><?php the_title(); ?></a></h2>
 	<div class="post"><?php the_content(); ?></div>
-	<?php edit_post_link('Edit this post.'); ?>
+	<?php edit_post_link('Edit this page.'); ?>
 </div>
 <?php get_footer(); ?>
