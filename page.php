713c7583d7db87ac0e2dda1cc5de9b3c516d7813
Paul Downey <pauldowney@Unknown-00-25-00-fa-00-70.home>
2011-01-08 11:25:08 +0000

    page edit

--- page.php
+++ page.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <div id="content">
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php the_post(); ?>
 	<h2><a href="/">Whatfettle</a> &#8275; <a href="<?php the_permalink()  ?>"><?php the_title(); ?></a></h2>
 	<div class="post"><?php the_content(); ?></div>
-<?php endwhile; endif; ?>
+	<?php edit_post_link('Edit this post.'); ?>
 </div>
 <?php get_footer(); ?>
