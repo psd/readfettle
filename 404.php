94d07052c0b2f83b47ea1213891987c8cb13ef26
Paul Downey <pauldowney@Unknown-00-25-00-fa-00-70.home>
2011-01-08 10:56:28 +0000

    search on 404

--- 404.php
+++ 404.php
@@ -1,17 +1,11 @@
 <?php header("HTTP/1.1 404 Not Found"); ?>
 <?php get_header(); ?>
-<div id="container">
-	<div id="content" class="widecolumn">
-		<div id="post-error404" class="post single-post">
-			<p class="post-date-single">{ Error 404 }</p>
-			<h2 class="post-title-single">Page Not Found</h2>
-			<div class="post-entry">
-				<p>Oh noes! We can't find that page! Maybe someone gave you a bum steer,
-				the Interweb tubes are blocked, or maybe I dropped the ball on this one. Go <a href="/">Home?</a></li>
-				</ul> 
-			</div>
-		</div>
-	</div>
+<div id="content">
+	<p>{ Error 404 }</p>
+	<h2><a href="/">Whatfettle</a> &#8275; Page Not Found</h2>
+	<p>Oh noes! We can't find that page! Maybe someone gave you a bum steer, the Interweb tubes are blocked, or maybe I dropped the ball on this one.</p>
+	<p>Perhaps you'd like to go <a href="/">home</a> or .. </p>
+	<?php include (TEMPLATEPATH . '/searchform.php'); ?>
 </div>
 </body>
 </html>
